f9ce854137f026efb780c8062536520a7cf58de8
Ed <eddyburt4@gmail.com>
2018-03-04 16:06:44 +0000

    adding in test - but need to mock api call really!

--- src/tests/suites/components/Routes_test.tsx
+++ src/tests/suites/components/Routes_test.tsx
@@ -1,26 +1,34 @@
-// import * as React from 'react';
-// import { shallow } from 'enzyme';
-// import { DisconnectedRoutes } from '../../../components/Routes';
-// import { IRoutesProps } from '../../../components/Routes/routesInterfaces';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { DisconnectedRoutes } from '../../../components/Routes';
+import { IRoutesProps } from '../../../components/Routes/routesInterfaces';
 
-// describe('Routes test suite', () => {
-//     test('Render logged out Routesbar', () => {
-//         const props: Readonly<IRoutesProps> = {
-//             isLoggedIn: false,
-//         };
-//         const RoutesScreen = shallow(
-//             <DisconnectedRoutes {...props} />,
-//         );
-//         expect(RoutesScreen).toMatchSnapshot();
-//     });
+describe('Routes test suite', () => {
+    test('Render logged out Routes', () => {
+        const props: Readonly<IRoutesProps> = {
+            isLoggedIn: false,
+            loggedInUser: null,
+        };
+        const RoutesScreen = shallow(
+            <DisconnectedRoutes {...props} />,
+        );
+        expect(RoutesScreen).toMatchSnapshot();
+    });
 
-//     test('Render logged in Routesbar', () => {
-//         const props: Readonly<IRoutesProps> = {
-//             isLoggedIn: true,
-//         };
-//         const RoutesScreen = shallow(
-//             <DisconnectedRoutes {...props} />,
-//         );
-//         expect(RoutesScreen).toMatchSnapshot();
-//     });
-// });
+    // Need to mock api call for this one! 
+    // test('Render logged in Routes', () => {
+    //     const props: Readonly<IRoutesProps> = {
+    //         isLoggedIn: true,
+    //         loggedInUser: {
+    //             userId: 'Ed',
+    //             token: 'token',
+    //             displayName: 'Ed',
+    //             email: 'email',
+    //         },
+    //     };
+    //     const RoutesScreen = shallow(
+    //         <DisconnectedRoutes {...props} />,
+    //     );
+    //     expect(RoutesScreen).toMatchSnapshot();
+    // });
+});
