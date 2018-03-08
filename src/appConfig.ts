a822883d05ce94c6c56870b883fd475727a7f9f5
Ed <eddyburt4@gmail.com>
2018-03-08 18:34:24 +0000

    organising app config (need better solution to this!)

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,16 +1,30 @@
-// Live setting
-// const baseURL = "http://82.25.191.210/HouseMoneyAPI/api/";
 
-// Local Setting
+// Local Settings
+
 const baseURL: string = 'http://localhost:5000/api/';
+export const houseMoneyUrl: string = 'http://localhost:3001/';
+export const houseFoodUrl: string = 'http://localhost:3002/';
 
-// Integration Test Setting
-// const baseURL: string = 'http://82.25.191.210/MyHouseTestAPI/api/';
 
-// Dev Staging Setting
-// const baseURL: string = 'http://82.25.191.210/MyHouseDevAPI/api/';
-
+// Integration Test Settings
+/*
+const baseURL: string = 'http://82.25.191.210/MyHouseTestAPI/api/';
 export const houseMoneyUrl: string = 'http://housemoney.surge.sh/';
 export const houseFoodUrl: string = 'http://housefood.surge.sh/';
+*/
+
+// Dev Staging Settings
+/*
+// const baseURL: string = 'http://82.25.191.210/MyHouseDevAPI/api/';
+export const houseMoneyUrl: string = 'http://housemoney.surge.sh/';
+export const houseFoodUrl: string = 'http://housefood.surge.sh/';
+*/
+
+// Live Settings
+/*
+const baseURL = "http://82.25.191.210/HouseMoneyAPI/api/";
+export const houseMoneyUrl: string = 'http://housemoney.surge.sh/';
+export const houseFoodUrl: string = 'http://housefood.surge.sh/';
+*/
 
 export default baseURL;
