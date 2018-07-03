9418415524bd46a441934ab0b030e649eec8f9a1
Ed <eddyburt4@gmail.com>
2018-07-03 18:50:03 +0100

    adding epic

--- src/main/epics.ts
+++ src/main/epics.ts
@@ -1,10 +1,12 @@
 import { combineEpics } from 'redux-observable';
+import addHouseholdRequestEpic from '../components/AddHousehold/addHouseholdEpic';
 import getHouseholdsOfUserRequestEpic from '../components/Households/householdsEpic';
 import newsFeedRequestEpic from '../components/NewsFeed/newsFeedEpic';
 
 const combinedEpics = combineEpics(
     getHouseholdsOfUserRequestEpic,
     newsFeedRequestEpic,
+    addHouseholdRequestEpic,
 );
 
 export default combinedEpics;
